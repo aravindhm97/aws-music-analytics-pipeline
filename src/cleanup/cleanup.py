fa54f34b7a6a4a5efa31d53586833c75e1cc0979
aravindhm97 <aravindhkumarm.1997@gmail.com>
2025-07-08 18:16:43 +0530

    Update cleanup.py

--- src/cleanup/cleanup.py
+++ src/cleanup/cleanup.py
@@ -1,16 +1,12 @@
 import boto3
 from datetime import datetime, timedelta
+
 def lambda_handler(event, context):
-    s3 = boto3.resource('s3')
-    bucket_name = "music-data-akm"
-    bucket = s3.Bucket(bucket_name)
-    deleted_count = 0
-    # Delete raw data files older than 2 days
+    bucket = boto3.resource('s3').Bucket("music-data-YOUR_INITIALS")
+    deleted = 0
+    # Delete files >2 days old
     for obj in bucket.objects.filter(Prefix='raw_data/'):
         if obj.last_modified < datetime.now(obj.last_modified.tzinfo) - timedelta(days=2):
             obj.delete()
-            deleted_count += 1
-    return {
-        "statusCode": 200,
-        "body": f"Deleted {deleted_count} objects from {bucket_name}/raw_data/"
-    }
+            deleted += 1
+    return {"deleted": deleted}

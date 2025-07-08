991df56a1ffe5871e7721936d2c1bb8d34a1c600
aravindhm97 <aravindhkumarm.1997@gmail.com>
2025-07-08 18:17:39 +0530

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,42 +2,46 @@ provider "aws" {
   region = "ap-south-1" # Free tier supported region
 }
 
-# S3 Data Lake
+# 1. S3 Data Lake
 resource "aws_s3_bucket" "music_data" {
-  bucket = "music-data-${var.your_initials}" # Change your_initials
-  acl    = "private"
+  bucket = "music-data-${var.your_initials}" # e.g., music-data-akm
 }
 
-# IAM Role for Lambda
-resource "aws_iam_role" "lambda_role" {
-  name = "music_lambda_role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      }
-    }]
-  })
-}
-
-# Lambda Function
+# 2. Lambda Functions
 resource "aws_lambda_function" "data_generator" {
-  function_name = "music_data_generator"
-  role          = aws_iam_role.lambda_role.arn
+  function_name = "music-data-generator"
   handler       = "app.lambda_handler"
   runtime       = "python3.8"
   s3_bucket     = aws_s3_bucket.music_data.id
-  s3_key        = "src/data_generator.zip"
+  s3_key        = "src/data_generator.zip" # Upload your ZIP here
 }
 
-# Glue Job
-resource "aws_glue_job" "music_etl" {
-  name     = "music_data_transformation"
-  role_arn = aws_iam_role.glue_role.arn
+# 3. Glue ETL Job
+resource "aws_glue_job" "etl_job" {
+  name = "music-etl"
   command {
-    script_location = "s3://${aws_s3_bucket.music_data.bucket}/src/glue_etl.py"
+    script_location = "s3://${aws_s3_bucket.music_data.id}/src/glue_etl.py"
   }
+}
+
+# 4. Step Functions Orchestration
+resource "aws_sfn_state_machine" "pipeline" {
+  definition = <<EOF
+{
+  "StartAt": "GenerateData",
+  "States": {
+    "GenerateData": {
+      "Type": "Task",
+      "Resource": "${aws_lambda_function.data_generator.arn}",
+      "Next": "RunETL"
+    },
+    "RunETL": {
+      "Type": "Task",
+      "Resource": "arn:aws:states:::glue:startJobRun",
+      "Parameters": {"JobName": "${aws_glue_job.etl_job.name}"},
+      "End": true
+    }
+  }
+}
+EOF
 }

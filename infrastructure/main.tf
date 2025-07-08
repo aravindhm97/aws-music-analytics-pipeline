19bff9df9e1782705434254f1b6fac8f9f732240
aravindhm97 <aravindhkumarm.1997@gmail.com>
2025-07-08 18:28:02 +0530

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -9,16 +9,19 @@ resource "aws_s3_bucket" "music_data" {
 
 # 2. Lambda Functions
 resource "aws_lambda_function" "data_generator" {
-  function_name = "music-data-generator"
+  function_name = "music-data-generator-${var.your_initials}"
+  role          = aws_iam_role.lambda_generator_role.arn
   handler       = "app.lambda_handler"
   runtime       = "python3.8"
+  timeout       = 10
   s3_bucket     = aws_s3_bucket.music_data.id
-  s3_key        = "src/data_generator.zip" # Upload your ZIP here
+  s3_key        = "src/data_generator.zip"
 }
 
 # 3. Glue ETL Job
-resource "aws_glue_job" "etl_job" {
-  name = "music-etl"
+resource "aws_glue_job" "music_etl" {
+  name     = "music-etl-${var.your_initials}"
+  role_arn = aws_iam_role.glue_execution_role.arn  # <-- CRITICAL MISSING LINE
   command {
     script_location = "s3://${aws_s3_bucket.music_data.id}/src/glue_etl.py"
   }
